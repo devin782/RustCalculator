6a289c524ae1f3a3d4156c955b60eb8870e39b35
devin782 <devin.wolford@oit.edu>
2021-04-21 21:13:10 -0700

    Added powers/sqrt/quadratic

--- Calculator/src/main.rs
+++ Calculator/src/main.rs
@@ -1,9 +1,5 @@
 mod math_functions;
 use math_functions::get_input;
-use math_functions::add;
-use math_functions::sub;
-use math_functions::mult;
-use math_functions::div;
 
 fn main() 
 {
@@ -11,28 +7,57 @@ fn main()
     let mut check_done: String;
 
     while done != true {
-        println!("Which calculation would you like to do?\n1: addition\n2: subtraction\n3: multiplication\n4: division");
-        let choice: i32 = get_input().trim().parse().unwrap();
+        println!("Which calculation would you like to do?
+        1: Addition
+        2: Subtraction
+        3: Multiplication
+        4: Division
+        5: Square
+        6: Cube
+        7: Square Root
+        8: Quadratic");
+            
+        let choice: u32 = get_input().trim().parse().unwrap();
 
         if choice == 1
         {
-            let answer = add();
+            let answer = math_functions::add();
             println!("Your answer is: {}", answer);
         }
         else if choice == 2
         {
-            let answer = sub();
+            let answer = math_functions::sub();
             println!("Your answer is: {}", answer);
         }
         else if choice == 3
         {
-            let answer = mult();
+            let answer = math_functions::mult();
             println!("Your answer is: {}", answer);
         }
         else if choice == 4
         {
-            let answer = div();
+            let answer = math_functions::div();
             println!("Your answer is: {}", answer);
+        }
+        else if choice == 5
+        {
+            let answer = math_functions::sqr();
+            println!("Your answer is: {}", answer);
+        }
+        else if choice == 6
+        {
+            let answer = math_functions::cube();
+            println!("Your answer is: {}", answer);
+        }
+        else if choice == 7
+        {
+            let answer = math_functions::sqr_root();
+            println!("Your answer is: {}", answer);
+        }
+        else if choice == 8
+        {
+            let answer = math_functions::quadratic();
+            println!("Zeros: {} and {} ", answer[0], answer[1])
         }
 
         println!("Are you done?");
--- Calculator/src/math_functions.rs
+++ Calculator/src/math_functions.rs
@@ -4,46 +4,90 @@ pub fn get_input() -> String {
     buffer
 }
 
-pub fn add() -> f32
+pub fn add() -> f64
 {
     println!("Input first value:");
-    let x: f32 = get_input().trim().parse().unwrap();
+    let x: f64 = get_input().trim().parse().unwrap();
     println!("Input second value:");
-    let y: f32 = get_input().trim().parse().unwrap();
+    let y: f64 = get_input().trim().parse().unwrap();
 
     let result = x + y;
     return result;
 }
 
-pub fn sub() -> f32
+pub fn sub() -> f64
 {
     println!("Input first value:");
-    let x: f32 = get_input().trim().parse().unwrap();
+    let x: f64 = get_input().trim().parse().unwrap();
     println!("Input second value:");
-    let y: f32 = get_input().trim().parse().unwrap();
+    let y: f64 = get_input().trim().parse().unwrap();
 
     let result = x - y;
     return result;
 }
 
-pub fn mult() -> f32
+pub fn mult() -> f64
 {
     println!("Input first value:");
-    let x: f32 = get_input().trim().parse().unwrap();
+    let x: f64 = get_input().trim().parse().unwrap();
     println!("Input second value:");
-    let y: f32 = get_input().trim().parse().unwrap();
+    let y: f64 = get_input().trim().parse().unwrap();
 
     let result = x * y;
     return result;
 }
 
-pub fn div() -> f32
+pub fn div() -> f64
 {
     println!("Input first value:");
-    let x: f32 = get_input().trim().parse().unwrap();
+    let x: f64 = get_input().trim().parse().unwrap();
     println!("Input second value:");
-    let y: f32 = get_input().trim().parse().unwrap();
+    let y: f64 = get_input().trim().parse().unwrap();
 
     let result = x / y;
+    return result;
+}
+
+pub fn sqr() -> f64
+{
+    println!("Input number:");
+    let x: f64 = get_input().trim().parse().unwrap();
+    let result = x*x;
+    return result;
+}
+
+pub fn cube() -> f64
+{
+    println!("Input number:");
+    let x: f64 = get_input().trim().parse().unwrap();
+    let result = x*x*x;
+    return result;
+}
+pub fn sqr_root() -> f64
+{
+    println!("Input number:");
+    let x: f64 = get_input().trim().parse().unwrap();
+    let result = x.sqrt();
+    return result;
+}
+
+pub fn quadratic() -> [f64; 2]
+{
+    println!("Input your a:");
+    let mut a: f64 = get_input().trim().parse().unwrap();
+    while a == 0.0
+    {
+        println!("Pick an a other than 0");
+        a = get_input().trim().parse().unwrap();
+    }
+    println!("Input your b:");
+    let b: f64 = get_input().trim().parse().unwrap();
+    println!("Input your c:");
+    let c: f64 = get_input().trim().parse().unwrap();
+
+    let zero_1 = (-b + (b*b - 4.0*a*c as f64).sqrt()) / (2.0*a);
+    let zero_2 = (-b - (b*b - 4.0*a*c as f64).sqrt()) / (2.0*a);
+
+    let result = [zero_1, zero_2];
     return result;
 }
